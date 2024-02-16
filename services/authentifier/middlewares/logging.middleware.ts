4deb4eb5c3900a81c4c740e70ec3b58f6a2985db
Corentin <69297384+CorentinTrt@users.noreply.github.com>
2024-02-16 10:27:29 -0500

    chore(authentifier): Add initial setup (#10)

---
+++ services/authentifier/middlewares/logging.middleware.ts
@@ -0,0 +1,21 @@
+import morgan from 'morgan';
+
+morgan.token('splitter', (req) => '\x1b[0m--------------------------------------------\x1b[0m\n');
+
+morgan.token('statusColor', (req, res, args) => {
+    const status = (typeof res.headersSent !== 'boolean' ? Boolean(res.header) : res.headersSent)
+        ? res.statusCode
+        : undefined
+
+    const color = status >= 500 ? 31
+        : status >= 400 ? 31
+            : status >= 300 ? 36
+                : status >= 200 ? 32
+                    : 0;
+
+    return '\x1b[' + color + 'm' + status + '\x1b[0m';
+});
+
+export const addLoggerMiddleware = (server) => {
+    server.use(morgan(`:splitter\x1b[0m:method\x1b[0m \x1b[36m:url\x1b[0m :statusColor - :date[clf] - :response-time ms -:user-agent`));
+}
