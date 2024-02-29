abcd91f8cb62b8058bb8e41f8f487f9088c9c78c
CorentinTrt <corentintruffaut@gmail.com>
2024-02-29 18:25:28 -0500

    build(userifier): Create docker stack

--- services/userifier/src/server.ts
+++ services/userifier/src/server.ts
@@ -5,12 +5,12 @@ import fs from 'fs';
 import { addResponseLoggerMiddleware } from './middlewares/response-logging.middleware';
 import { addGetRoutes } from './routes/routes';
 
-const PORT = 3001;
-const HOST = 'localhost';
+const HOST = process.env.NODE_HOST;
+const PORT = process.env.NODE_PORT;
 
 const serversOptions = {
-  key: fs.readFileSync('../../common/ssl-certificates/self-cert.key'),
-  cert: fs.readFileSync('../../common/ssl-certificates/self-cert.crt'),
+  key: fs.readFileSync('ssl-certificates/hoomalocal.key'),
+  cert: fs.readFileSync('ssl-certificates/hoomalocal.crt'),
 };
 
 const addMiddlewares = async (server: Server) => {
